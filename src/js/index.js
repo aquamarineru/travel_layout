1d00c9d972b3c9664ecd37c4aa963456e89a488e
Marina <m_ilyushina@hotmail.com>
2022-05-23 17:36:01 +0200

    hamb prob

--- src/js/index.js
+++ src/js/index.js
@@ -2,3 +2,20 @@ new Gallery(document.getElementById('gallery'), {
     margin: 10
 
 });
+window.addEventListener('DOMContentLoaded', () => {
+    const menu = document.querySelector('.menu'),
+    menuItem = document.querySelectorAll('.menu_item'),
+    hamburger = document.querySelector('.hamburger');
+
+    hamburger.addEventListener('click', () => {
+        hamburger.classList.toggle('hamburger_active');
+        menu.classList.toggle('menu_active');
+    });
+
+    menuItem.forEach(item => {
+        item.addEventListener('click', () => {
+            hamburger.classList.toggle('hamburger_active');
+            menu.classList.toggle('menu_active');
+        });
+    });
+});
